6893d23c031a1e1ae7b5b84a4b236befa8a70442
c-dez <hdezcruzcarlos@gmail.com>
2024-04-21 18:26:52 -0600

    added _movePlayer()

--- Player.gd
+++ Player.gd
@@ -10,13 +10,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_movePlayer(delta)
+
+
+func _movePlayer(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+
 	if Input.is_action_pressed("move_left"):
 		velocity.x += -1
+
 	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_down"):
 		velocity.y += -1
+
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
 	
+	if velocity.length() > 0 :
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
